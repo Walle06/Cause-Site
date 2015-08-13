6d7ccfd400556affd87bbe0a7f504563257a72dd
Waler Garcia <wall.e.006@gmail.com>
2015-08-13 18:37:30 -0400

    updated layouts

--- js/cause.js
+++ js/cause.js
@@ -2,5 +2,16 @@ $(document).ready(function(){
 	$("#slideDiv").animate({
 		right:"-=1300px",
 	}, 6000);
+	$("#fadePic").click( function(){
+
+		$("#fadePic").fadeOut(3000);
+	});	
+
+		/*$("#mainBody").ready(function(){
+		 $("#mainBody").fadeIn(5000);
+
+		});*/
+		
+	
 
 });
